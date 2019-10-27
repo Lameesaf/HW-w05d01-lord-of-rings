14672b73bd911624329e11fdcdf5ea907585420d
Lamees AlFallaj <lafallaj2@gmail.com>
2019-10-27 17:32:58 +0300

    edit the whole page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      const Movies =[
+        {
+          title:"The Fellowship of the Ring" ,
+          hours: 2 ,
+          minutes:58 ,
+        },
+        {
+          title:"The Two Towers" ,
+          hours: 2,
+          minutes: 59 ,
+        },
+        {
+          title:"The Return of the King" ,
+          hours: 3 ,
+          minutes: 21 ,
+        },
+      ]
+
+      const allMovie = Movies.map(movie=>{
+        return <Movie title={movie.title} hours={movie.hours} minutes= {movie.minutes} />
+      })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {allMovie}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
